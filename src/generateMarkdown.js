a2b193831afd05f9bcc8b2fbfaf0a99f65782a60
sergii-onyx <twilight.cympak@gmail.com>
2022-04-02 19:01:19 -0400

    add render license badge

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,6 +1,18 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// FUNCTION THAT RETURNS A LICENSE BADGE OR EMPTY STRING(IN CASE IF NO LICANSE)
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  }
+  else if (license === "Mozilla") {
+    return `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`
+  }
+  else if (license === "Unlicense") {
+    return `![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)`
+  }
+  else if (license === "I do not want to choose a license") {
+    return ""
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -3,19 +3,19 @@ const fs = require('fs');
 // CREATE A README.MD FILE AND SAVE IT TO "DIST" FOLDER IN MAIN FOLDER
 const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
-      fs.writeFile('./dist/README.md', fileContent, err => {
-        // in case of error reject the error
-        if (err) {
-          reject(err);
-          return;
-        }
-        // in case if writing of file complete show the corresponding message
-        resolve({
-          ok: true,
-          message: 'File created!'
+        fs.writeFile('./dist/README.md', fileContent, err => {
+            // in case of error reject the error
+            if (err) {
+                reject(err);
+                return;
+            }
+            // in case if writing of file complete show the corresponding message
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
         });
-      });
     });
-  };
+};
 
-  module.exports = writeFile;
+module.exports = writeFile;
