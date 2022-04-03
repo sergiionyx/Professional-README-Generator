366e3dc2897a322856c84cf78273a87c4088f797
sergii-onyx <twilight.cympak@gmail.com>
2022-04-03 10:38:07 -0400

    generate readme markdown

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -75,10 +75,51 @@ ${tests}`
   }
 };
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+// FUNCTION TO GENERATE A MARKDOWN FOR README
+const generateMarkdown = data => {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+- ${data.descr1}
+- ${data.descr2}
+- ${data.descr3}
+- ${data.descr4}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.about}
+
+${renderLicenseSection(data.license)}
+
+${renderBadges(data.confirmBadges, data.badges)}
+
+${renderFeatures(data.confirmFeatures, data.features)}
+
+${renderContribute(data.confirmContribute, data.contribute)}
+
+${renderTests(data.confirmTests, data.tests)}
+
+## Questions
+
+Here is link to my GitHub profile page: https://github.com/${data.username}
+
+And also you may contact me via e-mail: ${data.email}
 `;
 }
 
